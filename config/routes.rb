5c1a3091e784fc8e2a122d26e180ecdb6b6c80c4
Jaime Jazareno <jaimejazareno1996@gmail.com>
2017-04-19 11:27:42 +0800

    change in route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
 	post 'checkLogin' => 'account#checkLogin'
 	get 'result' => 'account#result'
 	get 'index' => 'account#index'
+
 	get 'register' => 'account#registerPage'
 	post 'register' => 'account#register'
+	get 'getData' => 'account#getData'
+	post 'postData' => 'account#postData'
 	root 'account#index'
 end
